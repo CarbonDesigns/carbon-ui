2295964f2bd06d177f2d1a5ee419ce3c90b2fc38
defr0zen <dennis.frostlander+defrosted@gmail.com>
2017-06-19 17:40:46 +0200

    added devserver cors (#67)

--- scripts/make-ui-config.js
+++ scripts/make-ui-config.js
@@ -318,6 +318,11 @@ module.exports = function (settings) {
                     }
                 ]
             },
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            },
             stats: {
                 colors: true,
                 timings: true,

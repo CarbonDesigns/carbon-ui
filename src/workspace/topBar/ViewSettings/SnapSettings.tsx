cdec7d64bfa6944b157077310370573bb2df4fdd
Alex <alex@carbonium.io>
2017-07-03 16:36:33 +0200

    snap settings and localization

--- src/workspace/topBar/ViewSettings/SnapSettings.tsx
+++ src/workspace/topBar/ViewSettings/SnapSettings.tsx
@@ -2,21 +2,22 @@ import React from "react";
 // import Hue from 'react-color/lib/components/common/Hue';
 // import Alpha from 'react-color/lib/components/common/Alpha';
 // import SliderHandle from "../../../shared/SliderHandle";
-import {Component} from "../../../CarbonFlux";
-import {FormattedMessage} from "react-intl";
+import { Component } from "../../../CarbonFlux";
+import { FormattedMessage } from "react-intl";
 
-import {app, util, CustomGuides, PropertyTracker, Invalidate, PatchType} from "carbon-core";
-import {FormHeading, FormLine, FormGroup}                 from "../../../shared/FormComponents"
-import {GuiInlineLabel, GuiCheckbox, GuiDropDown, GuiButton} from "../../../shared/ui/GuiComponents";
 
-import ViewSettingsPage     from "./ViewSettingsPage";
+import { app, util, CustomGuides, PropertyTracker, Invalidate, PatchType, UserSettings } from "carbon-core";
+import { FormHeading, FormLine, FormGroup } from "../../../shared/FormComponents"
+import { GuiInlineLabel, GuiCheckbox, GuiDropDown, GuiButton } from "../../../shared/ui/GuiComponents";
+
+import ViewSettingsPage from "./ViewSettingsPage";
 // import {FormHeading, FormLine, FormGroup}                 from "../../../shared/FormComponents"
 // import {GuiInlineLabel, GuiDropDown, GuiButton} from "../../../shared/ui/GuiComponents";
 // import EditableList from "../../../shared/EditableList";
 // import SimpleList from "../../../shared/SimpleList";
 
-export default class SnapSettings extends Component <any, any>{
-    constructor(props){
+export default class SnapSettings extends Component<any, any>{
+    constructor(props) {
         super(props);
 
         // var artboards = app.activePage.getAllArtboards();
@@ -24,17 +25,19 @@ export default class SnapSettings extends Component <any, any>{
         // var artboardIndex = artboards.indexOf(activeArtboard);
         // var artboard = artboardIndex === -1 ? artboards[0] : activeArtboard;
         //
+
+        var userSettings = app.userSettings();
         this.state = {
             // settings: Object.assign({}, app.props.customGuides)
-            settings: Object.assign({show: true}, {})
+            settings: userSettings.snapTo
         };
         // this.saveSettingsDebounced = util.debounce(this.saveSettings.bind(this), 200);
     }
 
-    showChanged = () =>{
+    showChanged = () => {
         var settings = Object.assign({}, this.state.settings);
-        settings.show = !settings.show;
-
+        settings.enabled = !settings.enabled;
+        this.setState({ settings });
         //this.saveSettings(settings);
     };
 
@@ -50,19 +53,57 @@ export default class SnapSettings extends Component <any, any>{
     //     PropertyTracker.propertyChanged.unbind(this, this.onPropsChanged);
     // }
 
-    render(){
-        return <ViewSettingsPage slug="snapping" heading="Snapping" switcher={true} checked={this.state.settings.show} onChange={this.showChanged} >
-            <FormGroup heading="Snap to">
-                <FormLine><GuiCheckbox label="columns"/></FormLine>
-                <FormLine><GuiCheckbox label="guides"/></FormLine>
-                <FormLine><GuiCheckbox label="grid"/></FormLine>
-                <FormLine><GuiCheckbox label="object corners"/></FormLine>
-                <FormLine><GuiCheckbox label="object centers"/></FormLine>
-                <FormLine><GuiCheckbox label="edge centers"/></FormLine>
+    _changeColumns = () => {
+        var settings = Object.assign({}, this.state.settings);
+        settings.columns = !settings.columns;
+        this.setState({ settings });
+    }
+
+    _changeGuides = () => {
+        var settings = Object.assign({}, this.state.settings);
+        settings.guides = !settings.guides;
+        this.setState({ settings });
+    }
+
+    _changeGrid = () => {
+        var settings = Object.assign({}, this.state.settings);
+        settings.grid = !settings.grid;
+        this.setState({ settings });
+    }
+
+    _changeVisibleObjects = () => {
+        var settings = Object.assign({}, this.state.settings);
+        settings.onlyVisibleObjects = !settings.onlyVisibleObjects;
+        this.setState({ settings });
+    }
+
+    _changeLockedObjects = () => {
+        var settings = Object.assign({}, this.state.settings);
+        settings.lockedObjects = !settings.lockedObjects;
+        this.setState({ settings });
+    }
+
+    render() {
+        return <ViewSettingsPage slug="snapping" heading="@snapping" switcher={true} checked={this.state.settings.enabled} onChange={this.showChanged} >
+            <FormGroup heading="@snap to">
+                <FormLine><GuiCheckbox label="@columns"
+                    onChange={this._changeColumns}
+                    checked={this.state.settings.snapToColumns} /></FormLine>
+                <FormLine><GuiCheckbox label="@guides"
+                    onChange={this._changeGuides}
+                    checked={this.state.settings.snapToGuides} /></FormLine>
+                <FormLine><GuiCheckbox label="@grid"
+                    onChange={this._changeGrid}
+                    checked={this.state.settings.snapToGrid} /></FormLine>
+                <FormLine><GuiCheckbox label="@only visible objects"
+                    onChange={this._changeVisibleObjects}
+                    checked={this.state.settings.snapToOnlyVisibleObjects} /></FormLine>
             </FormGroup>
 
             <FormGroup>
-                <FormLine><GuiCheckbox label="snap to locked objects"/></FormLine>
+                <FormLine><GuiCheckbox label="@snap to locked objects"
+                    onChange={this._changeLockedObjects}
+                    checked={this.state.lockedObjects} /></FormLine>
             </FormGroup>
         </ViewSettingsPage>
     }

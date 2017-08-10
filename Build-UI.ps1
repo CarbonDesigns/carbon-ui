503cc554fd6b3aac2db629ad22c38aba1664c1e1
defr0zen <dennis.frostlander+defrosted@gmail.com>
2017-08-10 21:50:35 +0200

    Deploying images
    * deploying images
    
    * build fix

--- Build-UI.ps1
+++ Build-UI.ps1
@@ -13,8 +13,8 @@ try
 
     New-Item .\target -ItemType "directory" -ErrorAction Ignore
     gci target -Exclude fonts,carbon-core*,carbon-api*,*.d.ts | ri -recurse
-    copy .\fonts\* .\target\fonts\ -Recurse
-    copy .\img\* .\target\img\ -Recurse
+    copy .\fonts\ .\target\ -Recurse
+    copy .\img\ .\target\ -Recurse
 
     if ($CopyCore)
     {

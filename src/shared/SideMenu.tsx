af2bc28e03c24b519d0f9af312ee9486a6613638
Alex <alex@carbonium.io>
2017-10-05 23:48:39 +0200

    side menu on small screens

--- src/shared/SideMenu.tsx
+++ src/shared/SideMenu.tsx
@@ -16,43 +16,53 @@ interface ISideMenuProps extends IReactElementProps {
 
 interface ISideMenuState {
     activeItemId: any;
+    active:boolean;
 }
 
 export class SideMenu extends React.Component<ISideMenuProps, ISideMenuState> {
     constructor(props) {
         super(props);
         var activeId = this.props.activeId;
-        if(!activeId && props.items.length) {
+        if (!activeId && props.items.length) {
             activeId = props.items[0].id;
         }
 
-        this.state = { activeItemId: activeId };
+        this.state = { activeItemId: activeId, active:false };
     }
 
     _itemClicked = (e) => {
         var id = e.currentTarget.dataset.itemId;
-        this.setState({ activeItemId: id });
+        this.setState({ activeItemId: id, active:false });
         if (this.props.onActiveChanged) {
             this.props.onActiveChanged(id);
         }
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
-        if(nextProps) {
-            this.setState({activeItemId:nextProps.activeId});
+        if (nextProps) {
+            this.setState({ activeItemId: nextProps.activeId, active:false });
         }
+    }
+
+    _toggleActive=()=> {
+        this.setState({active:!this.state.active})
     }
 
     render() {
         var items = this.props.items || [];
-        return <div className="side-menu">
-            {items.map(x => {
-                return <div key={x.id}>
-                    <span className={cx("side-menu_item", { "side-menu_item__active": (x.id) === this.state.activeItemId })} data-item-id={x.id} onClick={this._itemClicked}>
-                        <CarbonLabel id={x.label} />
-                    </span>
-                </div>
-            })}
+        return <div className={cx("side-menu", {active:this.state.active})}>
+            <div className="side-menu__activeselector form__heading fs-header" onClick={this._toggleActive}>
+                {items.filter(x=>x.id===this.state.activeItemId).map(x=><CarbonLabel tagName="h3" id={x.label}/>)}
+            </div>
+            <div className={cx("side-menu__items")}>
+                {items.map(x => {
+                    return <div key={x.id}>
+                        <span className={cx("side-menu_item", { "side-menu_item__active": (x.id) === this.state.activeItemId })} data-item-id={x.id} onClick={this._itemClicked}>
+                            <CarbonLabel id={x.label} />
+                        </span>
+                    </div>
+                })}
+            </div>
         </div>
     }
 }

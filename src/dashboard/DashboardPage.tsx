d5365df0c6f9daec18c3b185f69234be3d70166e
Alex <alex@carbonium.io>
2017-10-06 15:00:03 +0200

    changed dashboard to use side menu container

--- src/dashboard/DashboardPage.tsx
+++ src/dashboard/DashboardPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {dispatch, Component} from "../CarbonFlux";
+import {dispatch, listenTo, Component} from "../CarbonFlux";
 
 import {backend, DashboardProxy} from "carbon-api";
 import FolderTree from "./FolderTree";
 import ProjectList from "./ProjectList";
-import DashboardActions from "./DashboardActions";
 import TopMenu from "../shared/TopMenu";
+
+import DashboardStore from "./DashboardStore";
+import DashboardActions from "./DashboardActions";
+import { SideMenuContainer, ContentPage } from "../shared/SideMenuContainer";
 
 export default class DashboardPage extends Component<any, any>{
     static childContextTypes  = {
@@ -16,20 +19,44 @@ export default class DashboardPage extends Component<any, any>{
 
     constructor(props:any, context?:any) {
         super(props, context);
-        this.state = {};
+        this.state = {folders: [], activeFolderId:DashboardStore.state.get("activeFolderId")};
     }
+
+    @listenTo(DashboardStore)
+    _onDashboardStoreChanged(){
+        this.setState({folders: DashboardStore.state.get("folders"), activeFolderId:DashboardStore.state.get("activeFolderId")});
+    }
+
+    _onActiveFolderChanged = (folderId) => {
+        dispatch(DashboardActions.changeFolder(folderId));
+    };
 
     getChildContext() {
         return Object.assign({}, this.context, {companyId: this.state.companyId});
     }
 
+    private static _getFolderLabel(folder){
+        switch(folder.get("id")){
+            case "my":
+                return "@folders.my";
+            case "shared":
+                return "@folders.shared";
+            case "deleted":
+                return "@folders.deleted";
+            default:
+                return folder.get("name");
+        }
+    }
+
     render(){
+
         return <div className="dashboard-page light-page">
             <TopMenu location={this.props.location} dark={true} />
-            <section className="sidemenu-container">
-                <FolderTree/>
-                <ProjectList/>
-            </section>
+            <SideMenuContainer activePageId={this.state.activeFolderId} onActiveChanged={this._onActiveFolderChanged}>
+                {this.state.folders.map(folder=><ContentPage id={folder.get("id")} key={folder.get("id")} label={DashboardPage._getFolderLabel(folder)}>
+                        <ProjectList key={"list"+folder.get("id")} />
+                    </ContentPage>)}
+            </SideMenuContainer>
         </div>;
     }
 

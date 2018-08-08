be93735d0fa6f5f6281356b878d31d2f14207ec7
defr0zen <dennis@carbonium.io>
2018-08-08 08:02:34 +0200

    Fixed inline text edit

--- src/workspace/WorkspaceStore.ts
+++ src/workspace/WorkspaceStore.ts
@@ -1,7 +1,5 @@
 import { handles, CarbonStore, dispatchAction } from "../CarbonFlux";
-import {richApp} from '../RichApp';
 import CarbonActions from "../CarbonActions";
-
 import { app, ArtboardType } from "carbon-core";
 
 export default class WorkspaceStore extends CarbonStore {
@@ -17,16 +15,6 @@ export default class WorkspaceStore extends CarbonStore {
             else if (hasIcons) {
                 dispatchAction({type: "Library_Tab", area: "library", tabId: "2", async: true});
             }
-        }
-    }
-
-    @handles(CarbonActions.inlineEditModeChanged)
-    inlineEditModeChanged({mode}){
-        if (mode){
-            // HotKeyListener.suspend();
-        }
-        else{
-            // HotKeyListener.resume();
         }
     }
 }
